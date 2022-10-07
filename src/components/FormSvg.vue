762bc86143a1ca2e28bb6648cfdce61cdc38b1a7
Feuvrier-Eliot <95072076+Feuvrier-Eliot@users.noreply.github.com>
2022-10-07 14:08:47 +0200

    correction erreur formkit

--- src/components/FormSvg.vue
+++ src/components/FormSvg.vue
@@ -4,4 +4,33 @@
     import SvgProfil from "./SvgProfil.vue";
     import SvgDessus from "./SvgDessus.vue";
     const chaussure = ref<ChaussureSvg>({});
-   </script>
+</script>
+
+<template>
+    <div class="p-2">
+    <ul class="flex gap-1">
+    <li><a
+   href="#profil">Profil</a></li>
+    <li><a
+   href="#dessus">Dessus</a></li>
+    </ul>
+    <div class="carousel w-64">
+    <SvgProfil
+   class="carousel-item w-64"
+   v-bind="chaussure" id="profil" />
+    <SvgDessus
+   class="carousel-item w-64"
+   v-bind="chaussure" id="dessus" />
+    </div>
+<FormKit type="form" v-model="chaussure">
+<FormKit name="semelle" label="semelle" value="#FFFFFF" type="color" />
+<FormKit name="empeigne" label="empeigne" value="#FFFFFF" type="color" />
+<FormKit name="pointe" label="pointe" value="#FFFFFF" type="color" />
+<FormKit name="oeillet" label="oeillet" value="#FFFFFF" type="color" />
+<FormKit name="bande" label="bande" value="#FFFFFF" type="color" />
+<FormKit name="languette" label="languette" value="#FFFFFF" type="color" />
+<FormKit name="lacet" label="lacet" value="#FFFFFF" type="color" />
+<FormKit name="trimestre" label="trimestre" value="#FFFFFF" type="color" />
+</FormKit>
+</div>
+</template>
