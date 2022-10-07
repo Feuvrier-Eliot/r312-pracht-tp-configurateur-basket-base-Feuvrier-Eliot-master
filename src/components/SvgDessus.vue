bbd24123661cdb774c27e7dc9b36aacf6d8af8c5
Feuvrier-Eliot <95072076+Feuvrier-Eliot@users.noreply.github.com>
2022-10-07 13:58:41 +0200

    création du formkit

---
+++ src/components/SvgDessus.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+    import type { ChaussureSvg} from "@/types-svg";
+   withDefaults(defineProps<ChaussureSvg>(),{
+    semelle: "white",
+    empeigne: "white",
+    pointe: "white",
+    oeillet: "white",
+    bande: "white",
+    languette: "white",
+    lacet: "white",
+    trimestre: "white",
+   })
+   </script>
+   <template>
+   copié collé le code de vue dessus svg
+   </template>
